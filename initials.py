9eb0d0b78032de88230bb86e65d53e0b570decab
sena mac <64177981+Sena-Mac@users.noreply.github.com>
2026-05-31 04:33:53 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -2,6 +2,41 @@
 def main():
   pass #code goes here
 
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Mark Sena Agbenyegah
+# STUDENT ID:  2425403460
+# Email: senamalone4@gmail.com
+# Grader: Augustus
+#
+# On my honor, Mark Sena Agbenyegah, this programming assignment is my own work
+# and I have not provided this code to any other student
 
+print()
+print()
+def main():
+
+    #Printing my initials in small capital letters and then large block forms using each initial
+    print()
+    print("...MSA")
+    print()
+    print("...MMMM....MMMM........  SSSSSSSS  .............AA..........")
+    print("...MMMM....MMMM........ SSSSSSSSSSS............AAAA.........")
+    print("...MM.MM..MM.MM........SSSS.....SSS...........AAAAAA........")
+    print("...MM.MM..MM.MM........SSSS..................AAA..AAA.......")
+    print("...MMM.MMMM.MMM...........SSSS..............AAA....AAA......")
+    print("...MMM.MMMM.MMM..............SSSS..........AAAAAAAAAAAA.....")
+    print("...MMM..MM..MMM................SSSS........AAAAAAAAAAAA.....")
+    print("...MMM..MM..MMM........SSS.....SSSS........AAA......AAA.....")
+    print("...MMM..MM..MMM...**...SSSSSSSSSSS....**...AAA......AAA...**")
+    print("...MMM......MMM...**.....SSSSSSSS.....**...AAA......AAA...**")
+print()
+
+
+main()
+print()
+print()
 
 main()
